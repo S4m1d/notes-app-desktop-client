d25439b524512d978c938d507252de0da3e16c13
Dmitry Kolankov <kolankovda@student.bmstu.ru>
2022-05-11 23:39:34 +0300

    refactoring: removed spring as non-relevant and unused

--- app/src/main/java/ru/s4m1d/notes/app/desktop/client/app/runner/AppStarter.java
+++ app/src/main/java/ru/s4m1d/notes/app/desktop/client/app/runner/AppStarter.java
@@ -1,7 +1,5 @@
 package ru.s4m1d.notes.app.desktop.client.app.runner;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import ru.s4m1d.notes.app.desktop.client.app.configuration.InitialTasksConfiguration;
 import ru.s4m1d.notes.app.desktop.client.app.configuration.ModelConfiguration;
 import ru.s4m1d.notes.app.desktop.client.app.configuration.PresenterConfiguration;
@@ -12,8 +10,6 @@ import ru.s4m1d.notes.app.desktop.client.app.configuration.access.ViewApiAccess;
 
 import javax.swing.*;
 
-@Configuration
-@ComponentScan("ru.s4m1d")
 public class AppStarter {
 
     public static void createApp(){
